cb6f2dcad0f96d9494650ce5713085ec36ab0c21
Raildoc1 <Kirill1056@list.ru>
2020-10-31 20:32:34 +0700

    connection message

--- src/chat/ChatNode.java
+++ src/chat/ChatNode.java
@@ -1,10 +1,7 @@
 package chat;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.*;
 import java.util.Scanner;
 import java.util.UUID;
 
@@ -13,44 +10,74 @@ import java.util.UUID;
 
 public class ChatNode {
 
+    public enum MessageType {
+        CONF, // confirmation
+        TXT, // text message
+        CONN, // connection request
+        ALT // alternative node address
+    }
+
     //<editor-fold desc="private fields">
     private static final int TIME_OUT_MILLIS = 1000;
-    private static final int MAX_MSG_LENGHT = 256;
+    private static final int MAX_MSG_LENGTH = 256;
 
     private DatagramSocket socket;
     private InetAddress address;
     private String name;
-    private int portToListen;
+    private int parentPort;
+    private InetAddress parentAddress;
     private int port;
-    private byte[] buf = new byte[MAX_MSG_LENGHT];
+    private float lossChance;
+    private byte[] buf = new byte[MAX_MSG_LENGTH];
     private Scanner scanner = new Scanner(System.in);
+    private boolean isStopped = false;
     //</editor-fold>
 
     //<editor-fold desc="constructor">
-    public ChatNode(int port, String name, int portToListen) throws SocketException {
+    public ChatNode(String name, int port, int lossChance, byte[] parentIP, int parentPort) throws IOException {
         initSocket(port);
         this.port = port;
-        this.portToListen = portToListen;
+        this.parentPort = parentPort;
         this.name = name;
+        parentAddress = InetAddress.getByAddress(parentIP);
+        initLossChance(lossChance);
+
+        //byte[] addr = socket.getInetAddress().getAddress();
+
+        String connectionRequest = MessageType.CONN + ":127.0.0.1:" + port + "::";
+
+        DatagramPacket packet = new DatagramPacket(connectionRequest.getBytes(), connectionRequest.length(), parentAddress, parentPort);
+
+        socket.send(packet);
     }
 
-    public ChatNode(int port, String name) throws SocketException {
+    public ChatNode(String name, int port, int lossChance) throws SocketException, UnknownHostException {
         initSocket(port);
         this.name = name;
+        initLossChance(lossChance);
     }
 
-    private void initSocket(int port) throws SocketException {
+    private void initSocket(int port) throws SocketException, UnknownHostException {
         socket = new DatagramSocket(port);
         socket.setSoTimeout(TIME_OUT_MILLIS);
+    }
+
+    private void initLossChance (int lossChance) {
+        this.lossChance = ((float)lossChance) / ((float)100);
     }
     //</editor-fold>
 
     public void tick() {
+
+        if(isStopped) return;
+
         try {
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
             socket.receive(packet);
 
             String receivedMessage = new String(packet.getData());
+
+            System.out.println(receivedMessage.split("\\::")[0]);
 
             String[] split = receivedMessage.split("\\s+");
 
@@ -59,16 +86,25 @@ public class ChatNode {
             UUID uuid = UUID.randomUUID();
             String confirmationMessage = name + " " + uuid.toString() + " " + split[1];
 
+            System.out.println(packet.getData());
+
         } catch (IOException e) { /*IGNORE*/ }
 
         try {
             String msg = readStdin();
 
-            if(!msg.equals("")) {
-                DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
-                packet.setData(msg.getBytes());
-                socket.send(packet);
+            if(msg.isEmpty()) return;
+
+            if(msg.equals("stop")) {
+                System.out.println("Stopping...");
+                stop();
+                return;
             }
+
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
+            packet.setData(msg.getBytes());
+            socket.send(packet);
+
         } catch (IOException e) {
             System.out.println("Failed to read from stdin!");
         }
@@ -76,6 +112,11 @@ public class ChatNode {
 
     public void stop() {
         socket.close();
+        isStopped = true;
+    }
+
+    public boolean isStop() {
+        return isStopped;
     }
 
     private String readStdin() throws IOException {
--- src/chat/Main.java
+++ src/chat/Main.java
@@ -1,8 +1,48 @@
 package chat;
 
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
+
+        // <string : Name> <int : port> <int : loss chance>
+        // <string : Name> <int : port> <int : loss chance> <string : parent's ip> <int parent's port>
+
+        if(args.length != 3 && args.length != 5) {
+            System.out.println("Wrong arguments!");
+            return;
+        }
+
+        ChatNode node;
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            if(args.length == 3) {
+                node = new ChatNode(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+            } else {
+                byte[] addr = new byte[4];
+                String[] strAddr = args[3].split("\\.");
+
+                for (int i = 0; i < 4; i++) {
+                    addr[i] = Byte.parseByte(strAddr[i]);
+                }
+
+                node = new ChatNode(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]), addr, Integer.parseInt(args[4]));
+            }
+
+            while (true) {
+                if(!node.isStop()) node.tick();
+                else break;
+            }
+
+        } catch (SocketException | UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
