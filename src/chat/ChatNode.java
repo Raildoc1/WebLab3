a32391861fa32c3d9dd2666ea19a335b58a62f69
Raildoc1 <Kirill1056@list.ru>
2020-11-01 14:33:02 +0700

    Handle Messages

--- src/chat/ChatNode.java
+++ src/chat/ChatNode.java
@@ -2,6 +2,8 @@ package chat;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.UUID;
 
@@ -17,34 +19,70 @@ public class ChatNode {
         ALT // alternative node address
     }
 
+    private class Connection {
+        InetAddress address;
+        int port;
+        public Connection(InetAddress address, int port) {
+            this.address = address;
+            this.port = port;
+        }
+    }
+
+    private class PendingMessage {
+        String msg;
+        String uuid;
+        public PendingMessage(String msg, String uuid) {
+            this.msg = msg;
+            this.uuid = uuid;
+        }
+    }
+
     //<editor-fold desc="private fields">
     private static final int TIME_OUT_MILLIS = 1000;
     private static final int MAX_MSG_LENGTH = 256;
 
-    private DatagramSocket socket;
-    private InetAddress address;
+    // Node characteristics
     private String name;
+    private DatagramSocket socket;
+    private float lossChance;
+    private int port;
+
+    // Node connection
+    private boolean hasParent;
     private int parentPort;
     private InetAddress parentAddress;
-    private int port;
-    private float lossChance;
+    private int alternativeParentPort;
+    private InetAddress alternativeParentAddress;
+    private ArrayList<Connection> connections;
+
+    // Other
+    ArrayList<PendingMessage> pendingMessages;
     private byte[] buf = new byte[MAX_MSG_LENGTH];
     private Scanner scanner = new Scanner(System.in);
     private boolean isStopped = false;
+    private Random random = new Random();
     //</editor-fold>
 
     //<editor-fold desc="constructor">
     public ChatNode(String name, int port, int lossChance, byte[] parentIP, int parentPort) throws IOException {
-        initSocket(port);
+
+        hasParent = true;
+
         this.port = port;
         this.parentPort = parentPort;
         this.name = name;
         parentAddress = InetAddress.getByAddress(parentIP);
+        connections = new ArrayList<Connection>();
+        pendingMessages = new ArrayList<PendingMessage>();
+
+        initSocket(port);
         initLossChance(lossChance);
+        SendParentConnectionRequest();
 
-        //byte[] addr = socket.getInetAddress().getAddress();
+    }
 
-        String connectionRequest = MessageType.CONN + ":127.0.0.1:" + port + "::";
+    private void SendParentConnectionRequest() throws IOException {
+        String connectionRequest = MessageType.CONN + ":" + port + "::";
 
         DatagramPacket packet = new DatagramPacket(connectionRequest.getBytes(), connectionRequest.length(), parentAddress, parentPort);
 
@@ -52,8 +90,11 @@ public class ChatNode {
     }
 
     public ChatNode(String name, int port, int lossChance) throws SocketException, UnknownHostException {
-        initSocket(port);
+        hasParent = false;
         this.name = name;
+        connections = new ArrayList<Connection>();
+        pendingMessages = new ArrayList<PendingMessage>();
+        initSocket(port);
         initLossChance(lossChance);
     }
 
@@ -72,25 +113,64 @@ public class ChatNode {
         if(isStopped) return;
 
         try {
+
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
             socket.receive(packet);
 
             String receivedMessage = new String(packet.getData());
 
-            System.out.println(receivedMessage.split("\\::")[0]);
+            if(!receivedMessage.contains("::")) return;
 
-            String[] split = receivedMessage.split("\\s+");
+            //int lastOccur = receivedMessage.lastIndexOf("::");
 
+            //receivedMessage = receivedMessage.substring(0, lastOccur);
+
+            receivedMessage = receivedMessage.split("\\::")[0];
+
+            String[] split = receivedMessage.split("\\:", 2);
+
+            MessageType messageType = MessageType.valueOf(split[0]);
+
+            receivedMessage = split[1];
+
+            switch (messageType) {
+                case CONF:
+                    System.out.println("CONF: " + receivedMessage);
+                    HandleConfirmationMessage(receivedMessage);
+                    break;
+                case TXT:
+                    Double rand = random.nextDouble();
+                    //System.out.println(rand + "<" + lossChance);
+                    if(rand < lossChance) break;
+                    HandleMessage(receivedMessage, packet.getAddress(), packet.getPort());
+                    break;
+                case CONN:
+                    System.out.println("CONN: " + packet.getAddress() + " " + Integer.parseInt(receivedMessage));
+                    connections.add(new Connection(packet.getAddress(), Integer.parseInt(receivedMessage)));
+                    break;
+                case ALT:
+                    fillAlternativeNodeAddress(receivedMessage);
+                    break;
+                default:
+                    System.out.println(receivedMessage + " " + messageType);
+                    break;
+            }
+
+
+            /*
             packet.getData();
 
             UUID uuid = UUID.randomUUID();
             String confirmationMessage = name + " " + uuid.toString() + " " + split[1];
 
             System.out.println(packet.getData());
-
+            */
         } catch (IOException e) { /*IGNORE*/ }
 
         try {
+
+            SendPendingMessages();
+
             String msg = readStdin();
 
             if(msg.isEmpty()) return;
@@ -101,12 +181,124 @@ public class ChatNode {
                 return;
             }
 
-            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
-            packet.setData(msg.getBytes());
-            socket.send(packet);
+            SendMessageToAllNeighbors(MessageType.TXT, msg);
 
         } catch (IOException e) {
             System.out.println("Failed to read from stdin!");
+        }
+    }
+
+    private void HandleConfirmationMessage(String receivedMessage) {
+        int uuidIndex = receivedMessage.lastIndexOf(":");
+        String uuid = receivedMessage.substring(uuidIndex + 1);
+        for (PendingMessage msg: pendingMessages) {
+            if(msg.uuid.equals(uuid)) {
+                pendingMessages.remove(msg);
+                break;
+            }
+        }
+    }
+
+    private void HandleMessage(String receivedMessage, InetAddress address, int port) throws IOException {
+
+        int uuidIndex = receivedMessage.lastIndexOf(":");
+
+        String uuid = receivedMessage.substring(uuidIndex + 1);
+        String msg = receivedMessage.substring(0, uuidIndex);
+
+        String confMessage = "CONF:" + uuid + "::";
+
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
+        packet.setData(confMessage.getBytes());
+        socket.send(packet);
+
+        System.out.println("MSG: " + msg);
+    }
+
+    private void fillAlternativeNodeAddress(String receivedMessage) throws UnknownHostException {
+        String[] split;
+        split = receivedMessage.split("\\:");
+        alternativeParentPort = Integer.parseInt(split[1]);
+        byte[] addr = new byte[4];
+        String[] strAddr = split[0].split("\\.");
+        for (int i = 0; i < 4; i++) {
+            addr[i] = Byte.parseByte(strAddr[i]);
+        }
+        alternativeParentAddress = InetAddress.getByAddress(addr);
+    }
+
+    private void SendMessageToAllNeighbors(MessageType messageType, String msg) throws IOException {
+
+        msg = msg.replaceAll("\\:\\:", "[UNKNOWN_SYMBOL]");
+
+        msg = messageType + ":" + msg;
+
+        if(messageType == MessageType.TXT) {
+            UUID uuid = UUID.randomUUID();
+            msg +=  ":" + uuid.toString();
+            pendingMessages.add(new PendingMessage(msg, uuid.toString()));
+        }
+
+        DatagramPacket packet;
+        msg += "::";
+
+        SendMessageToAllNeighbors(msg);
+/*
+        if(hasParent){
+            packet = new DatagramPacket(buf, buf.length, parentAddress, parentPort);
+
+            System.out.println("Sending \"" + msg + "\" to" + parentAddress + ":" + parentPort);
+
+            packet.setData(msg.getBytes());
+            socket.send(packet);
+        }
+
+        if(connections.isEmpty()) return;
+
+        for (Connection c : connections) {
+            packet = new DatagramPacket(buf, buf.length, c.address, c.port);
+            System.out.println("Sending \"" + msg + "\" to" + c.address + ":" + c.port);
+            packet.setData(msg.getBytes());
+            socket.send(packet);
+        }
+        */
+    }
+
+    private void SendMessageToAllNeighbors(String msg) throws IOException {
+/*
+        if(messageType == MessageType.TXT) {
+            UUID uuid = UUID.randomUUID();
+            pendingMessages.add(new PendingMessage(msg, uuid.toString()));
+            msg += ":" + uuid.toString();
+        }
+
+        msg = messageType + ":" + msg + "::";
+*/
+        DatagramPacket packet;
+        if(hasParent){
+            packet = new DatagramPacket(buf, buf.length, parentAddress, parentPort);
+
+            System.out.println("Sending \"" + msg + "\" to " + parentAddress + ":" + parentPort);
+
+            packet.setData(msg.getBytes());
+            socket.send(packet);
+        }
+
+        if(connections.isEmpty()) return;
+
+        for (Connection c : connections) {
+            packet = new DatagramPacket(buf, buf.length, c.address, c.port);
+            System.out.println("Sending \"" + msg + "\" to" + c.address + ":" + c.port);
+            packet.setData(msg.getBytes());
+            socket.send(packet);
+        }
+    }
+
+    private void SendPendingMessages() throws IOException {
+        if(pendingMessages.isEmpty()) return;
+
+        for (PendingMessage msg : pendingMessages) {
+            SendMessageToAllNeighbors(msg.msg + "::");
         }
     }
 
